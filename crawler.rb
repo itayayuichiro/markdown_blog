33ab6c059f7060a03a22ca23ca265bb4d35aea7c
itayayuichiro <mr.itaya.magic@gmail.com>
2023-01-01 21:19:52 +0900

    fix: 修正

--- crawler.rb
+++ crawler.rb
@@ -9,7 +9,7 @@ class Crawler
     35.times { |i|
       page = agent.get("file:///Users/itayayuuichirou/Documents/src/markdown_blog/docs/article#{i+1}.html")
       doc = Nokogiri::HTML.parse(page.body.toutf8, nil, 'utf-8')
-      puts "- [#{doc.title}](./article#{i}.html)"
+      puts "- [#{doc.title}](./article#{i+1}.html)"
     }
   end
 end
